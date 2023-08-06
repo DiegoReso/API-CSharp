e47cfb1f875054b10b65cae9417b679bbea46d76
Diego Reis <diegoreso@me.com>
2023-08-05 23:40:08 -0300

    create the structure

---
+++ API-CRUD/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using API_CRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscartodosUsuarios()
+        {
+            return Ok();
+        }
+        
+            
+         
+    }
+}
---
+++ API-CRUD/Models/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿namespace API_CRUD.Models
+{
+    public class Tarefa
+    {
+
+        public int Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public int Status { get; set; }
+   
+
+    }
+}
